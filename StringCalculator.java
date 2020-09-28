a586e9a05cda6663bc64b09c674d5e28df62c8c8
Monika Targosz <37025393+MonikaTargosz@users.noreply.github.com>
2020-09-28 14:49:43 +0200

    Add files via upload

--- StringCalculator.java
+++ StringCalculator.java
@@ -1,44 +1,22 @@
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+public class StringCalculator {
 
-import static org.junit.jupiter.api.Assertions.*;
+    public int add(String input) {
 
-class StringCalculatorTest {
+        if (input.isEmpty()) {
+            return 0;
+        }
 
-    private StringCalculator calculator;
+        else {
+            String[] numbers = input.split(",");
+            int result = 0;
+            for (String singleNumber:numbers) {
+                result += getIntForm(singleNumber);
+            }
+            return result;
+        }
+    }
 
-    @BeforeEach
-    public void createCalculator() {
-
-        calculator = new StringCalculator();
-    }
-    @Test
-    void shouldReturnZeroWhenEmptyStringIsGiven() {
-        assertEquals(0,calculator.add(""));
-    }
-    @Test
-    void shouldReturnOneNumberWhenOneNumberIsGiven() {
-        assertEquals(2,calculator.add("2"));
-    }
-    @Test
-    void shouldReturnSumOfTheNumbersWhenTwoNumbersIsGiven() {
-        assertEquals(5,calculator.add("2,3"));
-        assertEquals(5,calculator.add("7,-2"));
-    }
-    @Test
-    void shouldReturnSumOfTheNumbersWhenThreeNumbersIsGiven() {
-        assertEquals(6,calculator.add("8,-2,0"));
-    }
-    @Test
-    void shouldReturnTrueWhenSumNumbersIsEqualsExpectedValue() {
-        int result=calculator.add("8,-2,0");
-        int expect=6;
-        boolean result2 = expect == result;
-        assertEquals(true,result2);
-    }
-    @Test
-    void assertsThatObjectIsNull() {
-        if(calculator==null)
-            assertNull(null);
+    private int getIntForm(String numbers){
+        return Integer.parseInt(numbers);
     }
 }
